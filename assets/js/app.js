22206645251f8f2bbfbf9aa235b69b795d0ac4c2
Andrzej Kurtycz <andrzejkurtycz@gmail.com>
2023-05-03 10:43:20 +0200

    first step, navigation

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+let navigation = document.querySelector(".navbar")
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 15)
+    {navigation.classList.add("navbar--move")}
+    else{navigation.classList.remove(navbar--move)}
+})

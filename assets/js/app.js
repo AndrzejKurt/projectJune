542dc12fdd8481dd81b809fa6b89cf502521f107
Andrzej Kurtycz <andrzejkurtycz@gmail.com>
2023-05-04 19:04:39 +0200

    first step

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
-let navigation = document.querySelector(".navbar")
+let navigation = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
-    if(window.pageYOffset > 15)
-    {navigation.classList.add("navbar--move")}
-    else{navigation.classList.remove(navbar--move)}
-})
+  if (window.pageYOffset > 0) {
+    navigation.classList.add("navbar--move");
+  } else {
+    navigation.classList.remove("navbar--move");
+  }
+});
+
+
 
